8209c04aa415a92f49f9ecb0539455a568517dd6
c0dead <anastasiaromankina1609@gmail.com>
2022-10-10 01:28:03 +0200

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/us-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know Us</h5>
+      <h2>About Us</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>4 years studying</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>be the first one</small>
+            </article>
+
+            <article className='about__card'>
+              <AiOutlineFolderOpen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>3 completed</small>
+            </article>
+          </div>
+
+            <p>
+              Hello, world! It's Anna and Anastasia. <br/> We are passionate creators with 4 years of development experience with programming languages and 2D animations. Skilled in designing user-friendly yet innovative games.
+            </p>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
